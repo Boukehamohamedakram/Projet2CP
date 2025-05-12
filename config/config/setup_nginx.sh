c4dd9b062b2ad060ae5f9a022249cd1406c92327
RootAccessX <aymenpmail@gmail.com>
2025-05-13 00:25:41 +0200

    Update setup_nginx.sh

--- config/config/setup_nginx.sh
+++ config/config/setup_nginx.sh
@@ -4,20 +4,14 @@
 sudo apt update
 sudo apt install -y nginx
 
-# Remove the default configuration (if any)
 sudo rm /etc/nginx/sites-enabled/default
 
-# Copy the Nginx configuration file to the correct location
 sudo cp ./config/nginx.conf /etc/nginx/sites-available/frontend
 
-# Enable the site configuration
 sudo ln -s /etc/nginx/sites-available/frontend /etc/nginx/sites-enabled/
 
-# Copy the built React frontend to the appropriate directory
 sudo cp -r ./Front\ end/build/* /var/www/frontend/
 
-# Restart Nginx to apply changes
 sudo systemctl restart nginx
 
-# Verify that Nginx is running
 sudo systemctl status nginx
