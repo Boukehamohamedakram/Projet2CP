1a11f3d8d5d4f61f7190aa1e7635155c94dad963
RootAccessX <aymenpmail@gmail.com>
2025-05-12 23:50:19 +0200

    Update setup_backend.sh

--- config/config/setup_backend.sh
+++ config/config/setup_backend.sh
@@ -1,18 +1,23 @@
 #!/bin/bash
 
-# Step 1: Navigate to the backend directory where manage.py is located
+
 cd "$(dirname "$0")/../Projet2CP/back end/BackQuiz"
 
-# Step 2: Create and activate the virtual environment
+
 python3 -m venv venv
 source venv/bin/activate
 
-# Step 3: Install backend dependencies from the requirements.txt file
+
 pip install --upgrade pip
 pip install -r "$(dirname "$0")/../Projet2CP/back end/requirements.txt"
 
-# Step 4: Run Django database migrations
-python manage.py migrate
 
-# Step 5: Run the Django development server
-python manage.py runserver 0.0.0.0:8000
+sudo cp "$(dirname "$0")/../systemd/new-backend.service" /etc/systemd/system/
+
+
+sudo systemctl daemon-reload
+sudo systemctl enable new-backend.service
+sudo systemctl start new-backend.service
+
+
+sudo systemctl status new-backend.service
