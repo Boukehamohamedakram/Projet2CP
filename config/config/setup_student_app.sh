0d017a2af3b70d7957ab5b63adcfff881233948e
RootAccessX <aymenpmail@gmail.com>
2025-05-13 09:04:35 +0200

    Update setup_student_app.sh

--- config/config/setup_student_app.sh
+++ config/config/setup_student_app.sh
@@ -17,10 +17,10 @@ echo "<!DOCTYPE html>
     <h1>Download the App</h1>
     <h2>Android</h2>
     <p>Click the link below to download the APK for Android:</p>
-    <a href='http://192.168.1.1/app.apk' download>Download for Android</a>
+    <a href='http://192.168.1.1/app-debug.apk' download>Download for Android</a>
     <h2>iOS</h2>
     <p>Click the link below to download the IPA for iOS:</p>
-    <a href='http://192.168.1.1/app.ipa' download>Download for iOS</a>
+    <a href='http://192.168.1.1/FlutterIpaExport%281%29.ipa' download>Download for iOS</a>
 </body>
 </html>" | sudo tee /var/www/html/index.html
 
